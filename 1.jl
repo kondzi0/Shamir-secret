aabc514619a9b629a4a471950952cb09e1e12f94
kondzi0 <kondzio.adamczyk@gmail.com>
2016-01-18 19:18:39 +0100

    Update 1.jl

--- 1.jl
+++ 1.jl
@@ -1,66 +1,117 @@
 
-function joinSecretPart(shares::Array, points::Array, primeNumber::Int64) 
-    #Funkcja do odczytu tajemnicy 
-    #Nic tylko podstawianie danych do wielomianu intetrpolacyjnego lagrange
+function generatePrimeNumber(moreThen::Int64)
+    #Liczba pierwsza musi być liczbą większa niż tajemnica,dlatego
+    #zwiększam o jeden i sprawdzam czy jest pierwsza, do czasu az napotkana 
+    #liczba jest liczba pierwsza
+        while !isprime(moreThen)
+            moreThen += 1
+        end
     
-    count = Int64
-    formula = Int64
-    startposition = Int64
-    nextposition = Int64
-    value = Int64
-    numerator = Int64
-    denumerator = Int64
-	accum = Int64
-    accum = 0
-    for formula=1:length(shares)
-
-        numerator = 1
-		denominator = 1
-        for count=1:length(shares)
-        
-           if formula == count 
-              continue 
-            end
-            
-            startposition = shares[formula]
-            nextposition = shares[count]
-            numerator = (numerator * (-nextposition)) % primeNumber
-            denominator = (denominator * (startposition - nextposition)) % primeNumber
-     
-            end
-        
-        value = points[formula]  
-        accum = (primeNumber + accum + (value * numerator *invmod(denominator,primeNumber))) % primeNumber
-		
-       end
-   
-    return accum
- end
-
- 
-function  makeFileForVisual(points::Array,shares::Array,primeNumber::Int64)
-  
-    f = open("/home/student/dane2.txt","w")
-    println(f,"x=$points")
-    println(f,"s=$shares")
-    println(f,"p=$primeNumber")
-       
-    close(f)
-    end
-
-
-function mainFunctionForJoinSecret(shares::Array, points::Array, primeNumber::Int64)
-    #Funkcja do łączenia udziałów i odczytu tajemnicy
-    joinSecretPart(points,shares,primeNumber)
-    
-    #Tworzenie pliku z danymi do wizualizacji
-   makeFileForVisual(points,shares,primeNumber)
-   include("odczyt.jl")
+    return moreThen
 end
 
-#Przykład wywołania funkcji
-shares = [22852903,12383261,16673571]
-points = [1,2,3]
-primeNumber = 24041263
+function generatePoints(needed::Int64, primeNumber::Int64,secret::Int64)
+    #Generujemy współczynniki wielomianu, losujemy ich needed - 1
+    #gdyż jako pierwszy współczynnik przyjmujemy tajemnice (secret)
+    #współczynniki musza byc mniejsze niz wygenerowana liczba pierwsza 
+    # primeNumber  - 1
+    points = Int64[]
+    push!(points,secret)
+    
+    while needed > length(points)
+        x = rand(1:primeNumber -1)
+         if !in(x,points)
+            push!(points,x)
+        end
+    end
+    
+    return points
+   
+end
 
-mainFunctionForJoinSecret(shares,points,primeNumber)
+function generateSecretPart(number::Int64, available::Int64, needed::Int64,coef::Array,prime::Int64) 
+    #Wyliczanie udziałów
+    #accum jest udziałem, tak więc podstawiamy wyliczone rzeczy pod wielomian i wyliczamy udział
+    #coef = tablica z współczynnikami
+        x = Int64
+        exp = Int64
+        c = Int64
+        accum = Int64
+        shares = Int64[]
+      
+        for x=1:available
+             accum = coef[1]
+            for exp=2:needed
+                accum = (accum  + (coef[exp] * ((x^(exp-1) ) % prime) % prime)) % prime
+            end 
+      
+        push!(shares, accum)
+        accum = 0
+        end
+       
+    return shares
+end
+
+function makeFileWithSecretPart(results::Array)
+    #Tworze pliki z udziałami do pliku secret(numerudzialu).txt
+    # w pliku znajduje sie w pierwszej lini "x"
+    # a w drugim sam udział
+    
+    for i=1:length(results)
+        f = open("/home/student/secret$i.txt","w")
+    
+        println(f,i)
+        println(f,results[i])
+        close(f)
+    end
+end
+
+
+function  makeFileForVisual(s::Int64,t::Int64,n::Int64,p::Int64,results::Array,points::Array,wTab::Array)
+    #Wpisywanie do pliku potrzebnych danych wymaganych do wizualizacji
+    f = open("/home/student/dane.txt","w")
+        println(f,"k=$s")
+        println(f,"n=$n")
+        println(f,"t=$t")
+        println(f,"p=$p")
+        println(f,"x=$points")
+        println(f,"s=$results")
+        println(f,"a=$wTab")
+        close(f)
+    end
+
+function sharingSecret(secret::Int64, avalible::Int64,needed::Int64)
+    # Główna funkcja programu
+    
+    p = Int64
+    point = Array
+    result = Array
+    
+    #generowanie liczby pierwszej
+    p = generatePrimeNumber(secret)
+    println("PrimeNumber: ",p)
+    
+    #generowanie punktów do wielomianu
+    point = generatePoints(needed, p,secret)
+    #println("Points: ",point)
+    
+    #obliczanie udziałów 
+    result = generateSecretPart(secret, avalible, needed, point,p)
+    #println("Result: ", result)
+    
+    #Tworzenie plików z udziałami
+    #makeFileWithSecretPart(result)
+    
+   wTab = Int64[]
+    for i=1:avalible
+        push!(wTab,i)
+    end
+    
+    #Tworzenie pliku do wizualizacji
+    makeFileForVisual(secret,avalible,needed,p,wTab,result,point)
+    println("Done!")
+    include("podzial.jl")
+end
+
+sharingSecret(2404123,6,3)
+
